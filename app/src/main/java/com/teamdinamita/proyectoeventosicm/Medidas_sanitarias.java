efc52c6ef378035aee3985f6e436be05e4ae15e5
Karen89K <karentvd10@gmail.com>
2021-11-24 17:58:48 -0700

    Registrar evento consulta

---
+++ app/src/main/java/com/teamdinamita/proyectoeventosicm/Medidas_sanitarias.java
@@ -0,0 +1,14 @@
+package com.teamdinamita.proyectoeventosicm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Medidas_sanitarias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medidas_sanitarias);
+    }
+}
